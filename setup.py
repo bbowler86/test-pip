d1c13f2b99e3cbea1c3161fd49b5357f38d73c20
Bowler <bbowler@tmp.com>
2021-06-09 10:46:38 -0400

    second commit

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name='test-pip-package',
+setup(name='testpip',
       version='0.0.1',
       description='Test Pip Package',
       author='Brian J. Bowler',
       url='https://localhost',
-      packages=['test-pip-package'],
+      packages=['testpip'],
       install_requires=['nltk'])
